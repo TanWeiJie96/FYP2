ff9ed309256e8e248c0523a96847395c331a123c
crystalweii <unified_lighter@hotmail.com>
2015-11-17 21:59:22 +0800

    continue

---
+++ FYPJ/Assets/Script/Collision/CheckForCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckForCollision : MonoBehaviour {
+    public bool Collided = false;
+    public bool noMoreCheckNeed = false; //stop checking wheter collide or not
+
+    public int slot;
+	// Use this for initialization
+	void Start () {
+        Global.gameEndSystem.winCondition.Add(Collided);
+        slot = Global.gameEndSystem.winCondition.Count - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!noMoreCheckNeed && collider.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            Collided = true;
+            Debug.Log("check point hit");
+        }
+    }
+}
---
+++ FYPJ/Assets/Script/Collision/CheckPointSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class CheckPointSystem : MonoBehaviour {
+    public List<GameObject> CheckPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int x = 0; x < CheckPoints.Count; ++x)
+        {
+            GameObject go = CheckPoints[x];
+            CheckForCollision Coc = go.GetComponent<CheckForCollision>();
+            if (Coc.Collided == true && Coc.noMoreCheckNeed == false)
+            {
+                Global.gameEndSystem.winCondition[Coc.slot] = Coc.Collided;
+                Global.gameEndSystem.needsCheckForGameEnd = true;
+                Coc.noMoreCheckNeed = true;
+                Coc.Collided = false;
+                Debug.Log("check point updated");
+            }
+        }
+
+	}
+}
