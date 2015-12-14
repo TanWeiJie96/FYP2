d32656264f6312ca898436edeb8c1b0856daeacb
TanWeiJie96 <syndicatepanda@hotmail.com>
2015-12-14 14:36:43 -0800

    added Basic speed boost power up

--- FYPJ/Assets/Script/Power Ups/CollectPowerUp.cs
+++ FYPJ/Assets/Script/Power Ups/CollectPowerUp.cs
@@ -4,16 +4,41 @@ using System;
 
 public class CollectPowerUp : OnColReactTemplete
 {
+	public bool changeSpe = false;
+	public GameObject speedUp;
+	public float timer = 1.0f;
 
-    public override void onTriEnterPower(Collider other)
-    {
-        Debug.Log("wtf");
+	public override void onTriEnter(Collider other)
+    {	
         if (other.tag == "Speed")
-        {
-            Debug.Log("Speed Power Up Got!");
+        {	
+			changeSpe = true;
+			speedUp.SetActive(false);
+
+			Debug.Log ("Timer: " + timer);
         }
     }
 
+	void Update()
+	{
+		if (changeSpe == true)
+			timer -= Time.deltaTime;
+
+		if (timer <= 0.0f)
+		{
+			timer = 0.0f;
+			changeSpe = false;
+		}
+	}
+
+//	public override void onTriExit(Collider other)
+//	{
+//		if (other.tag == "Speed")
+//		{
+//			changeSpe = false;
+//			speedUp.SetActive(false);
+//		}
+//	}
 }
 
 //public class CollectPowerUp : MonoBehaviour {
