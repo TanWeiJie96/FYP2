8610c941cfa86c810cd0c7f2b5d6404e54a9a67d
TanWeiJie96 <syndicatepanda@hotmail.com>
2016-01-04 02:09:06 -0800

    ???

---
+++ FYPJ/Assets/Script/ObjectRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotation : MonoBehaviour {
+
+	public float rotatePerMin = 10.0f;
+
+	void Update()
+	{
+		transform.Rotate (0, 6*rotatePerMin*Time.deltaTime, 0);
+	}
+}
