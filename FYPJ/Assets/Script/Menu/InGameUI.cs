f1069c28dd000db5682b8cfd68ac2217233cb663
Slife <crostycrost@gmail.com>
2016-01-28 22:40:43 +0800

    okay can

---
+++ FYPJ/Assets/Script/Menu/InGameUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameUI : MonoBehaviour {
+    public UIHandler scoreUI;
+
+    public UIHandler timerUI;
+    public Timer timerClass;
+
+    public UIHandler levelUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
